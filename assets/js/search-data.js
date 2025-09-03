0fc2ff67712ec88e621d4fd392ccf63dd6d6b879
russluber <102501423+russluber@users.noreply.github.com>
2025-09-03 19:04:20 +0000

    Deploying to gh-pages from @ russluber/russluber.github.io@9a3b50310d6a926b9733e5839b980f260648b68e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,14 +17,14 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-blog",
-          title: "blog",
+          title: "Blog",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/blog/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
