0c9463733152c8c772aea9e58cd09de434e14e2b
russluber <102501423+russluber@users.noreply.github.com>
2025-09-04 21:30:06 +0000

    Deploying to gh-pages from @ russluber/russluber.github.io@b1a40e7319a8c6aecfa19f0504dd59e463de8257 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -85,7 +85,7 @@ ninja.data = [{
         title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://github.com/", "_blank");
+          window.open("https://github.com/russluber", "_blank");
         },
       },{
         id: 'social-linkedin',
