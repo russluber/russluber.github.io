57f027bbecec8c7caebc9d6f7caff56368acfc50
russluber <102501423+russluber@users.noreply.github.com>
2025-09-05 00:08:16 +0000

    Deploying to gh-pages from @ russluber/russluber.github.io@a0c993234a72191df93209d88f04fbb4f9f262d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -57,18 +57,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-fliptop-analysis",
+            },},{id: "projects-fliptop-analysis",
           title: 'FlipTop Analysis',
           description: "Exploring the world of Filipino battle rap",
           section: "Projects",handler: () => {
